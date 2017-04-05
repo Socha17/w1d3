8ea4bd95792f73539cafac3778f3fcc8c8e30c71
Socha17 <mattsocha11@gmail.com>
2017-04-05 17:33:21 +0000

    counting Chars

--- char_counting.js
+++ char_counting.js
@@ -1,7 +1,22 @@
+var args = process.argv.slice(2);
+var objectChar = {}
 
+var countLetters = function(allLetters) {
 
-var countLetters = function() {
+  for (var i =0; i < allLetters.length; i++) {
 
+    // allLetters = allLetters.replace(/\s/g, '');
 
+   if (objectChar[allLetters[i]]) {
+      objectChar[allLetters[i]] += 1
+    } else {
+      objectChar[allLetters[i]] = 1
+    }
+  }
+
+return objectChar
 
 }
+
+
+console.log(countLetters(args.join('')));
